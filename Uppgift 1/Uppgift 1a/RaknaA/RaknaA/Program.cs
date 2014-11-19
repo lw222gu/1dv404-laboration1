7b0bf2975f981be304c6131514d40d0518f40529
Lisa Westlund <lw222gu@student.lnu.se>
2014-11-19 15:50:04 +0100

    Skrivit källkoden till 1a

--- Uppgift 1/Uppgift 1a/RaknaA/RaknaA/Program.cs
+++ Uppgift 1/Uppgift 1a/RaknaA/RaknaA/Program.cs
@@ -10,6 +10,28 @@ namespace RaknaA
     {
         static void Main(string[] args)
         {
+            string inputString;
+            int aLowerCase = 0;
+            int aUpperCase = 0;
+
+            Console.Write("Mata in en valfri textrad i vilken du vill beräkna antalet a och A: ");
+            inputString = Console.ReadLine();
+
+            foreach (char x in inputString) 
+            {
+                if(x == 'a')
+                {
+                    aLowerCase++;
+                }
+
+                if (x == 'A')
+                {
+                    aUpperCase++;
+                }
+            }
+
+            Console.WriteLine("Antal a: {0}", aLowerCase);
+            Console.WriteLine("Antal A: {0}", aUpperCase);
         }
     }
 }
