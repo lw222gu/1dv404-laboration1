8a50639982ae4e6f3190c3b8c1d34e81863dcdec
Lisa Westlund <lw222gu@student.lnu.se>
2014-11-19 22:32:40 +0100

    1b klar, program och dokument skrivet

---
+++ Uppgift 1/Uppgift 1b/RaknaSiffror/RaknaSiffror/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaknaSiffror
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            int countZero = 0;
+            int countEven = 0;
+            int countOdd = 0;
+
+            Console.WriteLine("Mata in ett heltal: ");
+            input = Console.ReadLine();
+
+            foreach (var x in input)
+            {
+                if (x.ToString() == "0")
+                {
+                    countZero++;
+                }
+                
+                else if (x % 2 != 0) 
+                {
+                    countOdd++;
+                }
+
+                else if (x % 2 == 0)
+                {
+                    countEven++;
+                }
+
+                
+            }
+
+            Console.WriteLine("Antal nollor: {0}", countZero);
+            Console.WriteLine("Antal udda: {0}", countOdd);
+            Console.WriteLine("Antal jämna: {0}", countEven);
+
+        }
+    }
+}
