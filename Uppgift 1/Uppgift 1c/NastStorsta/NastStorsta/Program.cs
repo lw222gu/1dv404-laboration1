01380f36a0a320b0e5aa96974c33caa310684b62
Lisa Westlund <lw222gu@student.lnu.se>
2014-11-22 17:52:09 +0100

    Programmet 1c klart

--- Uppgift 1/Uppgift 1c/NastStorsta/NastStorsta/Program.cs
+++ Uppgift 1/Uppgift 1c/NastStorsta/NastStorsta/Program.cs
@@ -13,7 +13,54 @@ namespace NastStorsta
 
             int largestNumber = 0;
             int secondLargestNumber = 0;
+            int input;
+            int numbers;
 
+            Console.Write("Ange hur många tal du vill mata in: ");
+            numbers = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= numbers; i++)
+            {
+                Console.Write("Ange tal {0}: ", i);
+                input = int.Parse(Console.ReadLine());
+
+                if(i == 1)
+                {
+                    largestNumber = input;
+                }
+
+                if (i == 2)
+                {
+                    if (input > largestNumber)
+                    {
+                        secondLargestNumber = largestNumber;
+                        largestNumber = input;
+                    }
+
+                    else
+                    {
+                        secondLargestNumber = input;
+                    }
+                }
+
+                else
+                {
+                    if (input < largestNumber && input > secondLargestNumber)
+                    {
+                        secondLargestNumber = input;
+                    }
+
+                    else if (input > largestNumber)
+                    {
+                        secondLargestNumber = largestNumber;
+                        largestNumber = input;
+                    }
+                }
+
+                
+            }
+
+            Console.WriteLine("Det näst största talet är {0}.", secondLargestNumber);
         }
     }
 }
