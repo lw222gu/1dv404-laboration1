c62b95fb38d6bc009beee81b39d7fc46060975cd
Lisa Westlund <lw222gu@student.lnu.se>
2014-11-23 19:01:46 +0100

    Börjat på uppgift 3b

---
+++ Uppgift 3/Uppgift 3b/FractionsCalculator/FractionsCalculator/Fraction.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FractionsCalculator
+{
+    class Fraction
+    {
+        private int _numerator;
+        private int _denominator;
+
+        public int getNumerator
+        {
+            get { return _numerator; }
+            set { _numerator = value; }
+        }
+
+        public int getDenominator
+        {
+            get { return _denominator; }
+            set { _denominator = value; }
+        }
+
+        public Fraction()
+        {
+            getNumerator = _numerator;
+            getDenominator = _denominator;
+        }
+
+        public bool isNegative(int numerator, int denominator)
+        {
+            if (numerator < 0 || denominator < 0)
+            {
+                return true;
+            }
+
+            else 
+            {
+                return false;
+            }
+        }
+
+    }
+}
