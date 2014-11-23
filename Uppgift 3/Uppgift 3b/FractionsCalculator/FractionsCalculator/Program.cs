6d2d74aef58d5cf54f3e2bb78f6016664d0d7dd0
Lisa Westlund <lw222gu@student.lnu.se>
2014-11-23 21:19:27 +0100

    Börjat skriva programfilen för 3b

--- Uppgift 3/Uppgift 3b/FractionsCalculator/FractionsCalculator/Program.cs
+++ Uppgift 3/Uppgift 3b/FractionsCalculator/FractionsCalculator/Program.cs
@@ -10,6 +10,25 @@ namespace FractionsCalculator
     {
         static void Main(string[] args)
         {
+            int numerator1;
+            int denominator1;
+            int numerator2;
+            int denominator2;
+
+            Console.Write("Ange nämnaren i det första bråket: ");
+            numerator1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ange täljaren i det första bråket: ");
+            denominator1 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ange nämnaren i det andra bråket: ");
+            numerator2 = int.Parse(Console.ReadLine());
+
+            Console.Write("Ange täljaren i det andra bråket: ");
+            denominator2 = int.Parse(Console.ReadLine());
+
+
+
         }
     }
 }
